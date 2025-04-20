9a646726e77717e39641be2b2d1db8ee016f4ca9
Subhan <sheikhanwer30@gmail.com>
2025-04-20 21:58:23 +0500

    logo added

--- src/components/LogoAnimation.tsx
+++ src/components/LogoAnimation.tsx
@@ -5,14 +5,31 @@ import { motion } from 'framer-motion'
 import Image from 'next/image';
 
 const images = [
-    { src: "/udemy.png", alt: "udemy" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
+    { src: "/sp.png", alt: "Social Pulse" },
+    { src: "/giaic.png", alt: "Governor Initiative for Artificial Intelligence, Web 3.0 and Metaverse" },
 ]
 
 const LogoAnimation = () => {
     return (
-        <div className='py-8 bg-purple-200/10 opacity-80'>
+        <div className='py-8 bg-purple-200/10 opacity-80 glass'>
             <div className='container mx-auto'>
-                <div className='overflow-hidden [mask-image: linear-gradient(to_right,_transparent,_black_25%,_black_75%,_transparent) ]'>
+                <div className='overflow-hidden [mask-image:linear-gradient(to_right,_transparent,_black_25%,_black_75%,_transparent)]'>
                     <motion.div
                         className='flex gap-14 flex-none pr-14'
                         animate={{
@@ -30,8 +47,9 @@ const LogoAnimation = () => {
                                 key={index}
                                 src={image.src}
                                 alt={image.alt}
-                                height={30}
-                                width={30}
+                                height={100}
+                                width={100}
+                                className='w-auto h-12'
                             />
                         ))}
 
