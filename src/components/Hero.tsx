d0595b5355586e7df25d9da82ee9e926d6b315bf
Subhan-Anwer <subhananwersheikh@gmail.com>
2026-01-04 18:01:35 +0500

    Added Resume/CV Download button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import { animate, motion, useMotionTemplate, useMotionValue } from 'framer-motio
 import React, { useEffect } from 'react'
 import Image from 'next/image';
 import { FiArrowRight } from 'react-icons/fi';
+import Link from 'next/link';
 
 const COLORS_TOP = ["#1367C6", "#13FFAA", "#CE84CF", "#DD335C"]
 
@@ -24,86 +25,98 @@ const Hero = () => {
 
 
   return (
-    <motion.section id='home'
+    <motion.section
+      id="home"
       style={{
-        backgroundImage
+        backgroundImage,
       }}
       className="relative grid min-h-screen place-content-center overflow-hidden px-4 py-24 text-gray-200"
     >
-
-      <div className='z-10 flex flex-col items-center text-center'>
-        <span className='sm:mb-4 mb-5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 mt-2 sm:text-sm text-xs'>
+      <div className="z-10 flex flex-col items-center text-center">
+        <span className="sm:mb-4 mb-5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 mt-2 sm:text-sm text-xs">
           Open for Work
         </span>
 
-        <p className='text-white/40 text-3xl sm:text-5xl md:text-7xl font-black sm:mb-1 mb-0'>Hi, I am</p>
-        <h1 className='max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black sm:leading-tight leading-[42px] text-transparent text-4xl sm:text-5xl md:text-7xl'>Subhan Anwer</h1>
+        <p className="text-white/40 text-3xl sm:text-5xl md:text-7xl font-black sm:mb-1 mb-0">
+          Hi, I am
+        </p>
+        <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black sm:leading-tight leading-[42px] text-transparent text-4xl sm:text-5xl md:text-7xl">
+          Subhan Anwer
+        </h1>
         {/* SEO tags */}
         <p className="sr-only">
-          Frontend Developer in Karachi, Website Developer, Next.js Expert, Tailwind CSS UI Designer, JavaScript Coder, Subhan Anwer Portfolio
+          Frontend Developer in Karachi, Website Developer, Next.js Expert,
+          Tailwind CSS UI Designer, JavaScript Coder, Subhan Anwer Portfolio
         </p>
 
         <Image
-          src='/profilepic.png'
-          alt='Subhan Anwer profile photo - frontend website developer'
+          src="/profilepic.png"
+          alt="Subhan Anwer profile photo - frontend website developer"
           width={250}
           height={250}
         />
-        <div className='flex flex-col sm:flex-row bg-white/10 shadow-xl p-3 rounded-3xl justify-center items-center sm:space-x-2 gap-3 mb-4 sm:mt-0 mt-6'>
+        <div className="flex flex-col sm:flex-row bg-white/10 shadow-xl p-3 rounded-3xl justify-center items-center sm:space-x-2 gap-3 mb-4 sm:mt-0 mt-6">
           <div className="flex items-center justify-center space-x-2">
             <Image
-              src='/obj1.png'
-              alt='object'
+              src="/obj1.png"
+              alt="object"
               width={30}
               height={30}
-              className='rounded-2xl mx-auto'
+              className="rounded-2xl mx-auto"
             />
             <Image
-              src='/obj1.png'
-              alt='object'
+              src="/obj1.png"
+              alt="object"
               width={30}
               height={30}
-              className='rounded-2xl mx-auto'
+              className="rounded-2xl mx-auto"
             />
             <Image
-              src='/obj1.png'
-              alt='object'
+              src="/obj1.png"
+              alt="object"
               width={30}
               height={30}
-              className='rounded-2xl mx-auto'
+              className="rounded-2xl mx-auto"
             />
           </div>
           <p>80+ Happy Clients</p>
         </div>
 
-        <p className='my-6 max-w-xl '>Frontend & Website Developer from Karachi, with 2+ years of experience</p>
+        <p className="my-6 max-w-xl ">
+          Frontend & Website Developer from Karachi, with 2+ years of experience
+        </p>
 
-        <motion.button
-          style={{
-            border,
-            boxShadow
-          }}
-          whileHover={{
-            scale: 1.015
-          }}
-          whileTap={{
-            scale: 0.985
-          }}
-          className='flex w-fit items-center gap-2 rounded-full px-4 py-2'
+        <Link
+          href="/SUBHAN_ANWER_RESUME.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          download
         >
-          Download CV
-          <FiArrowRight />
-        </motion.button>
-
+          <motion.button
+            style={{
+              border,
+              boxShadow,
+            }}
+            whileHover={{
+              scale: 1.015,
+            }}
+            whileTap={{
+              scale: 0.985,
+            }}
+            className="flex w-fit items-center gap-2 rounded-full px-4 py-2"
+          >
+            Download CV
+            <FiArrowRight />
+          </motion.button>
+        </Link>
       </div>
 
-      <div className='bg-circle-container'>
-        <div className='bg-circle-background'></div>
-        <div className='bg-circle'></div>
+      <div className="bg-circle-container">
+        <div className="bg-circle-background"></div>
+        <div className="bg-circle"></div>
       </div>
-
     </motion.section>
-  )
+  );
 }
 
 export default Hero
