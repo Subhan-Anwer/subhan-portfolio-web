c25e978913bde3604b0fc0f1fe78cc7c90ebf6c5
Subhan-Anwer <subhananwersheikh@gmail.com>
2026-01-07 10:29:29 +0500

    Create getPersonalDetails.ts

---
+++ src/sanity/lib/getPersonalDetails.ts
@@ -0,0 +1,22 @@
+import { defineQuery } from "next-sanity";
+import { sanityFetch } from "./live";
+
+export const getPersonalDetails = async () => {
+  const PERSONAL_DETAILS_QUERY = defineQuery(`
+            *[_type == "personalDetails"][0]
+            
+        `);
+
+  try {
+    //use Sanity Fetch to send the query
+    const personalDetails = await sanityFetch({
+      query: PERSONAL_DETAILS_QUERY,
+    });
+
+    //Return the list of personal details or an empty array if none found
+    return personalDetails.data || [];
+  } catch (error) {
+    console.log("Error fetching personal details:", error);
+    return [];
+  }
+};
